1d744bb9442d178dbb01a6c7b904cb080eb5f60d
eDraagen <90107745+eDraagen@users.noreply.github.com>
2023-02-08 08:25:36 +0100

    Updated script

--- app/webscrapeFinn_no.py
+++ app/webscrapeFinn_no.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-
-search = input("Type what you are searching")
+import time
 
 #//Fix this function so it puts the correct search "format" when sending it to getUrl function. Replace " " with "+"\\
 def searchWord(user_input):
-    pass
+    f = user_input.replace(" ", "+").lower()
+    getUrl(f)
 
 #//Now finding the correct link to search\\
 def getUrl(query):
@@ -22,8 +22,8 @@ def getContent(query_url):
     
     resultElements = result.find_all("article", class_="ads__unit")
     articleContent = result.find_all("div", class_="ads__unit__content")
-    # pricing = result.find_all("div", class_="ads__unit__img__ratio__price")
-    print(len(resultElements))
+    print("Found ", len(resultElements), " Adverts on current search")
+    time.sleep(3)
     for content in resultElements:
         title = content.find("h2", class_="ads__unit__content__title").text.strip()
         pricing = content.find("div", class_="ads__unit__img__ratio__price").text.strip()
@@ -33,4 +33,6 @@ def getContent(query_url):
         print(f"{link}\n")
         print()
 
-getUrl(search)
+if __name__ == "__main__":
+    search = input("Type what you are searching")
+    searchWord(search)
